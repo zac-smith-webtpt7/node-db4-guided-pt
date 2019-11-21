bf1d3320914be895a20f996a16871674ec5afd11
Zac Smith <zrs3141592@gmail.com>
2019-11-20 21:09:39 -0700

    create migration

---
+++ data/migrations/20191120205529_create-tables.js
@@ -0,0 +1,46 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable("zoos", tbl => {
+      tbl.increments();
+      tbl.string("zoo_name", 255).notNullable();
+      tbl
+        .string("address", 255)
+        .notNullable()
+        .unique();
+    })
+    .createTable("species", tbl => {
+      tbl.increments();
+      tbl
+        .string("species_name", 255)
+        .notNullable()
+        .unique();
+    })
+    .createTable("animals", tbl => {
+      tbl.increments();
+      tbl.string("animal_name", 255).notNullable();
+      tbl
+        .integer("species_id")
+        .unsigned()
+        .notNullable()
+        .references("species.id");
+      // .references('id')
+      // .inTable('species')
+    })
+    .createTable("zoo_animals", tbl => {
+      tbl
+        .integer("zoo_id")
+        .unsigned()
+        .notNullable()
+        .references("zoo.id");
+
+      tbl
+        .intger("animal_id")
+        .unsigned()
+        .notNullable()
+        .reference("animals.id");
+
+      tbl.primary(["zoo_id", "animal_id"]);
+    });
+};
+
+exports.down = function(knex) {};
